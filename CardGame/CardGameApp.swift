dc3aea0fd63bd0f3cb35530ed8232e916f41e045
Jim Walejko <jwalejko@me.com>
2026-03-23 20:49:28 -0700

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Jim Walejko on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
